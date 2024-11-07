e922c2af4b2a051ebbb5bf111dc962983bc5fef7
Joris Pevcevičius <joris.pevcas@gmail.com>
2024-11-07 22:13:48 +0200

    feat: splitting backuper config

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,7 @@
       ./wm
       ./neovim
       ./gaming
+      ./backuper
     ];
 
   wmconfig.users = [ "joris" "gaming" ];
@@ -113,32 +114,6 @@
     description = "Joris";
     extraGroups = [ "networkmanager" "wheel" ];
     useDefaultShell = true;
-  };
-
-  fileSystems."/mnt/nfs" = {
-    device = "/dev/disk/by-uuid/6ee83677-4096-4a6f-8795-dc23a16dfb4b";
-    fsType = "ext4";
-  };
-
-  users.users.backuper = {
-    isSystemUser = true;
-    uid = 500;
-    group = "backuper";
-    shell = pkgs.bashInteractive;
-    description = "Restic Backups";
-    createHome = true;
-    openssh.authorizedKeys.keyFiles = [ ./ssh/thinkpad_yubikey.pub ];
-  };
-  users.groups.backuper.gid = 500;
-
-  services.openssh = {
-    enable = true;
-    ports = [ 22 ];
-    settings = {
-      PasswordAuthentication = false;
-      AllowUsers = [ "backuper" ];
-      PermitRootLogin = "no";
-    };
   };
 
   home-manager.users.joris = { pkgs, ... }:
