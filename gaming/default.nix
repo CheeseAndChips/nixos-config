1bbe41b009c460c2f678228e8730d0f682cc7f49
Joris Pevcevičius <joris.pevcas@gmail.com>
2025-03-14 21:03:09 +0200

    amdgpu opencl

--- gaming/default.nix
+++ gaming/default.nix
@@ -28,5 +28,14 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+    extraPackages = with pkgs; [ rocmPackages.clr.icd ];
   };
+
+  boot.initrd.kernelModules = [ "amdgpu" ];
+  systemd.tmpfiles.rules = [
+    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
+  ];
+  environment.systemPackages = with pkgs; [
+    clinfo
+  ];
 }
